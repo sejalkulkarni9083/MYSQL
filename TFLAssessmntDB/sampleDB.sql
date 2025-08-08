6cab64a6ac48dfd7ef8653b17d1ca7515bc9b4db
sejal kulkarni <sejalkulkarni017@gmail.com>
2025-08-08 09:56:32 -0700

    database file

--- TFLAssessmntDB/sampleDB.sql
+++ TFLAssessmntDB/sampleDB.sql
@@ -421,3 +421,39 @@ INSERT INTO candidateanswers(candidateid, testquestionid,answerkey)VALUES(1,27,'
 INSERT INTO candidateanswers(candidateid, testquestionid,answerkey)VALUES(1,18,'d');
 INSERT INTO candidateanswers(candidateid, testquestionid,answerkey)VALUES(1,19,'b');
 INSERT INTO candidateanswers(candidateid, testquestionid,answerkey)VALUES(1,20,'b');
+
+INSERT INTO candidateanswers(candidateid, testquestionid,answerkey)VALUES(2,1,'b');
+INSERT INTO candidateanswers(candidateid, testquestionid,answerkey)VALUES(2,2,'a');
+INSERT INTO candidateanswers(candidateid, testquestionid,answerkey)VALUES(2,3,'b');
+INSERT INTO candidateanswers(candidateid, testquestionid,answerkey)VALUES(2,4,'a');
+INSERT INTO candidateanswers(candidateid, testquestionid,answerkey)VALUES(2,5,'a');
+INSERT INTO candidateanswers(candidateid, testquestionid,answerkey)VALUES(2,6,'c');
+INSERT INTO candidateanswers(candidateid, testquestionid,answerkey)VALUES(2,7,'a');
+INSERT INTO candidateanswers(candidateid, testquestionid,answerkey)VALUES(2,8,'d');
+INSERT INTO candidateanswers(candidateid, testquestionid,answerkey)VALUES(2,9,'c');
+INSERT INTO candidateanswers(candidateid, testquestionid,answerkey)VALUES(2,10,'d');
+
+
+
+ INSERT INTO candidateanswers(candidateid, testquestionid,answerkey)VALUES(2,11,'a');
+INSERT INTO candidateanswers(candidateid, testquestionid,answerkey)VALUES(2,12,'b');
+INSERT INTO candidateanswers(candidateid, testquestionid,answerkey)VALUES(2,13,'b');
+INSERT INTO candidateanswers(candidateid, testquestionid,answerkey)VALUES(2,14,'b');
+INSERT INTO candidateanswers(candidateid, testquestionid,answerkey)VALUES(2,15,'a');
+INSERT INTO candidateanswers(candidateid, testquestionid,answerkey)VALUES(2,16,'c');
+INSERT INTO candidateanswers(candidateid, testquestionid,answerkey)VALUES(2,17,'b');
+INSERT INTO candidateanswers(candidateid, testquestionid,answerkey)VALUES(2,18,'b');
+INSERT INTO candidateanswers(candidateid, testquestionid,answerkey)VALUES(2,19,'b');
+INSERT INTO candidateanswers(candidateid, testquestionid,answerkey)VALUES(2,20,'a');
+
+ 
+INSERT INTO candidateanswers(candidateid, testquestionid,answerkey)VALUES(3,1,'c');
+INSERT INTO candidateanswers(candidateid, testquestionid,answerkey)VALUES(3,2,'c');
+INSERT INTO candidateanswers(candidateid, testquestionid,answerkey)VALUES(3,3,'c');
+INSERT INTO candidateanswers(candidateid, testquestionid,answerkey)VALUES(3,4,'c');
+INSERT INTO candidateanswers(candidateid, testquestionid,answerkey)VALUES(3,5,'c');
+INSERT INTO candidateanswers(candidateid, testquestionid,answerkey)VALUES(3,6,'c');
+INSERT INTO candidateanswers(candidateid, testquestionid,answerkey)VALUES(3,7,'c');
+INSERT INTO candidateanswers(candidateid, testquestionid,answerkey)VALUES(3,8,'c');
+INSERT INTO candidateanswers(candidateid, testquestionid,answerkey)VALUES(2,9,'c');
+INSERT INTO candidateanswers(candidateid, testquestionid,answerkey)VALUES(3,10,'d');
