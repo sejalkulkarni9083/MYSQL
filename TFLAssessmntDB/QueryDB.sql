5033b52854ec0a28e807b36ecbfd2f1bac6bc75e
sejal kulkarni <sejalkulkarni017@gmail.com>
2025-08-13 08:44:43 -0700

    QueryDB updated

--- TFLAssessmntDB/QueryDB.sql
+++ TFLAssessmntDB/QueryDB.sql
@@ -40,3 +40,56 @@ SELECT s.title AS subject_title, qb.id AS question_bank_id, qb.title AS question
 FROM questionbank qb
 INNER JOIN evaluationcriterias ec ON qb.evaluationcriteriaid = ec.id
 INNER JOIN subjects s ON qb.subjectid = s.id;
+
+select questionbank.id, questionbank.title, subjects.title as subject ,evaluationcriterias.title as criteria
+from questionbank, subjects,evaluationcriterias
+where questionbank.subjectid=subjects.id and questionbank.evaluationcriteriaid=evaluationcriterias.id
+and subjects.id=1 and evaluationcriterias.id=1;
+                            
+select employees.firstname,employees.lastname,interviews.candidateid from employees
+inner join interviews
+where employees.id=interviews.candidateid
+order by interviews.candidateid asc;
+
+select interviews.candidateid,interviews.smeid,subjectmatterexperts.subjectid,subjects.id,subjects.title
+from interviews
+inner join subjectmatterexperts
+on interviews.smeid = subjectmatterexperts.id
+inner join subjects on subjectmatterexperts.subjectid=subjects.id;
+
+select interviews.candidateid,subjects.title
+from interviews
+inner join subjectmatterexperts
+on interviews.smeid = subjectmatterexperts.id
+inner join subjects on subjectmatterexperts.subjectid=subjects.id
+where interviews.candidateid=2;
+
+
+select interviews.id,interviews.interviewdate,interviews.interviewtime,interviews.smeid,
+concat(employees.firstname," ",employees.lastname)as SmeName from interviews
+inner join subjectmatterexperts 
+on interviews.smeid=subjectmatterexperts.id 
+inner join employees
+on subjectmatterexperts.employeeid= employees.id
+where interviews.id=2;
+
+select interviews.candidateid, concat(employees.firstname," ",employees.lastname)as CandidateName from employees
+inner join interviews
+on interviews.candidateid = employees.id
+where interviews.id=2;
+
+select subjects.id, subjects.title from interviews
+inner join subjectmatterexperts
+on interviews.smeid = subjectmatterexperts.id
+inner join subjects
+on subjectmatterexperts.subjectid = subjects.id
+where interviews.id=2;
+
+select evaluationcriterias.id, evaluationcriterias.title from interviews
+inner join interviewcriterias
+on interviews.id = interviewcriterias.interviewid
+inner join evaluationcriterias
+on interviewcriterias.evaluationcriteriaid = evaluationcriterias.id
+where interviews.id=2;
+            
+update questionbank set answerkey="c" where id = 1;
