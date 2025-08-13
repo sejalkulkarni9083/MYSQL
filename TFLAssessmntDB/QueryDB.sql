5f2a355c47c198ba3c73b38aec4a82c97e7763c8
sejal kulkarni <sejalkulkarni017@gmail.com>
2025-08-13 07:26:40 -0700

    QueryDB updated

--- TFLAssessmntDB/QueryDB.sql
+++ TFLAssessmntDB/QueryDB.sql
@@ -15,3 +15,28 @@ select questionbank.id,questionbank.title,questionbank.a,questionbank.b,question
 where subjects.title="ADVJAVA";
 
 select * from tests ;
+
+select * from testquestions;
+
+SELECT qb.*
+FROM questionbank qb
+INNER JOIN testquestions tq ON qb.id = tq.questionbankid
+WHERE tq.testid = 7;
+
+
+
+select * from questionbank where subjectid=(select id from subjects where title ="ADVJAVA");
+ 
+select * from questionbank inner join testquestions on testquestions.questionbankid = questionbank.id where testquestions.testid=1;
+
+INSERT INTO candidateanswers (candidateid, testquestionid, answerkey) VALUES (@candidateId, @testQuestionId, @answerKey);
+
+select * from candidateanswers;
+
+update candidatetestresults set testendtime =@testendtime where candidateid=@candidateid and testid=@testid;
+update candidatetestresults set testendtime ="2015-11-05 14:35:00" where candidateid=2 and testid=1;
+
+SELECT s.title AS subject_title, qb.id AS question_bank_id, qb.title AS question_title, ec.title AS criteria_title
+FROM questionbank qb
+INNER JOIN evaluationcriterias ec ON qb.evaluationcriteriaid = ec.id
+INNER JOIN subjects s ON qb.subjectid = s.id;
